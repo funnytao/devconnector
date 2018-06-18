e0e0267d10469f37f3a5d027939f1b41b1da5750
Tao Liu <taoliu404@gmail.com>
2018-06-18 14:38:30 -0400

    Completed login/register

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,26 @@
+import { GET_ERRORS, SET_CURRENT_USER } from '../actions/types';
+import { SSL_OP_DONT_INSERT_EMPTY_FRAGMENTS } from 'constants';
+import isEmpty from '../validation/isEmpty';
+
+const initialState = {
+  isAuthenticated: false,
+  user: {}
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_ERRORS:
+      return {
+        ...state,
+        user: action.payload
+      };
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
+    default:
+      return state;
+  }
+}
